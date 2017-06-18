0071e834c5fa6bd491561ed09041092daa2b0481
Brose Johnstone <baby-bell@users.noreply.github.com>
2017-06-17 23:19:44 -0700

    Order events by date

--- src/event.rs
+++ src/event.rs
@@ -1,14 +1,30 @@
+use std::collections::BTreeSet;
+use std::cmp::Ordering;
+
 use chrono::DateTime;
 use chrono::offset::local::Local;
 
 type Time = DateTime<Local>;
 
+#[derive(Debug, PartialEq, Eq)]
 struct Event {
     name: String,
     location: Option<String>,
     time: Time,
 }
 
+impl PartialOrd for Event {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.time.cmp(&other.time))
+    }
+}
+
+impl Ord for Event {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.time.cmp(&other.time)
+    }
+}
+
 pub struct EventTable {
-    events: Vec<Event>,
+    events: BTreeSet<Event>,
 }
