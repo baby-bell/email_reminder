4e2fb09f7d5c6168ce636785bdb27913b6e0a096
Brose Johnstone <baby-bell@users.noreply.github.com>
2017-06-17 22:11:13 -0700

    Add custom error type

---
+++ src/error.rs
@@ -0,0 +1,45 @@
+use lettre::transport::smtp::error::Error as SmtpError;
+use serde_json::error::Error as JsonError;
+
+use std::error::Error as StdError;
+use std::io::Error as IoError;
+use std::fmt;
+
+#[derive(Debug)]
+pub enum Error {
+    Smtp(SmtpError),
+    File(IoError),
+    Serde(JsonError),
+}
+
+impl StdError for Error {
+    fn description(&self) -> &str {
+        use Error::*;
+        use SmtpError::*;
+        match self {
+            &Smtp(ref err) => {
+                match err {
+                    &Transient(_) => "transient SMTP error",
+                    &Permanent(_) => "permanent SMTP error",
+                    &ResponseParsing(_) => "error parsing a response",
+                    &ChallengeParsing(_) => "error parsing a base64 string in response",
+                    &Client(_) => "internal client error",
+                    &Resolution => "DNS resolution error",
+                    &Io(_) => "I/O error",
+                }
+            }
+            &File(ref err) => err.description(),
+            &Serde(ref err) => err.description(),
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &Smtp(ref err) => write!(f, "{:?}", err)?,
+            &File(ref err) => write!(f, "{}", err)?,
+            &Serde(ref err) => write!(f, "{}", err)?,
+        }
+    }
+}
