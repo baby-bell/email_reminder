05b928c8701008cdafbd2f29cc925958ad2020b6
Brose Johnstone <baby-bell@users.noreply.github.com>
2017-06-18 18:28:26 -0700

    Rework event datastructures

--- src/event.rs
+++ src/event.rs
@@ -1,36 +1,65 @@
 use std::cmp::Ordering;
+use std::rc::Rc;
+use std::collections::{BTreeSet, HashMap, Bound};
+use std::collections::btree_set::Range;
 
 use chrono::DateTime;
 use chrono::offset::local::Local;
 
 type Time = DateTime<Local>;
 
-#[derive(Debug, PartialEq, Eq)]
-struct Event {
-    name: String,
-    location: Option<String>,
+#[derive(Debug, PartialEq, Eq, Clone, Hash, PartialOrd, Ord)]
+pub struct Event {
     time: Time,
-    reminders: Vec<Time>,
+    name: Rc<String>,
+    location: Option<Rc<String>>,
 }
 
-impl PartialOrd for Event {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Event {
-    fn cmp(&self, other: &Self) -> Ordering {
-        if self.time == other.time {
-            if self.location == other.location {
-                return self.name.cmp(&other.name);
-            }
-            return self.location.cmp(&other.location);
+impl Event {
+    pub fn new(name: String, location: Option<String>, time: Time) -> Self {
+        Event {
+            name: Rc::new(name),
+            location: location.map(Rc::new),
+            time: time,
         }
-        return self.time.cmp(&other.time);
     }
 }
 
 pub struct EventTable {
     events: BTreeSet<Event>,
+    reminders: HashMap<Event, Vec<Time>>,
+}
+
+pub struct EventIter<'a> {
+    inner: Range<'a, Event>,
+}
+
+impl<'a> Iterator for EventIter<'a> {
+    type Item = &'a Event;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next()
+    }
+}
+
+impl EventTable {
+    pub fn new() -> Self {
+        EventTable {
+            events: BTreeSet::new(),
+            reminders: HashMap::new(),
+        }
+    }
+
+    pub fn add_event(&mut self, e: Event) {
+        self.events.insert(e.clone());
+        self.reminders.insert(e, vec![]);
+    }
+
+    pub fn events_in_date_range<'a>(&'a self, start: Time, end: Time) -> EventIter<'a> {
+        let range_start = Bound::Included(Event::new("".into(), None, start));
+        let range_end = Bound::Excluded(Event::new("".into(), None, end));
+
+        EventIter {
+            inner: self.events.range((range_start, range_end)),
+        }
+    }
 }
