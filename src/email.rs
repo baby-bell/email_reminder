741dd4f8c4d5a779aa52e614520eb668ca6e67c4
Brose Johnstone <baby-bell@users.noreply.github.com>
2017-06-23 10:48:10 -0700

    Use higher security for emails

--- src/email.rs
+++ src/email.rs
@@ -1,10 +1,11 @@
-use lettre::transport::smtp::{SmtpTransportBuilder, SmtpTransport};
+use lettre::transport::smtp::{SmtpTransportBuilder, SmtpTransport, SecurityLevel};
 use super::Settings;
 use error::Error;
 
 pub fn connect(settings: &Settings) -> Result<SmtpTransport, Error> {
     let builder = SmtpTransportBuilder::new("smtp.gmail.com:587")?
         .encrypt()
+        .security_level(SecurityLevel::AlwaysEncrypt)
         .credentials(&settings.username, &settings.password);
     Ok(builder.build())
 }
