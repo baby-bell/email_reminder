b9b28dfd9ff35354e2e71e01dd4511301419ce70
Brose Johnstone <baby-bell@users.noreply.github.com>
2017-06-23 21:39:54 -0700

    Switch to rocket

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
 extern crate chrono;
 extern crate lettre;
-extern crate iron;
-#[macro_use]
-extern crate router;
-
-use iron::prelude::*;
-use iron::status;
-use iron::middleware::Handler;
-use router::Router;
-
+extern crate rocket;
 extern crate serde;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
+
+use rocket::State;
 
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -35,43 +32,11 @@ pub struct Settings {
     pub serving_port: u16,
 }
 
-struct State {
-    table: Mutex<EventTable>,
-}
-
-impl State {
-    pub fn new() -> Self {
-        State {
-            table: Mutex::new(EventTable::new()),
-        }
-    }
-}
-
-impl Handler for State {
-    fn handle(&self, req: &mut Request) -> IronResult<Response> {
-        let dispatcher = req.extensions.get::<Router>().unwrap();
-        if let Some(ref name) = dispatcher.find("name") {
-            Ok(Response::with((status::Ok,
-            serde_json::to_string(&self.table.lock().unwrap().events_by_name(name)).unwrap())))
-        } else {
-            Ok(Response::with(status::Ok))
-        }
-    }
-}
-
-fn handler(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Hello World")))
-}
-
 fn main() {
-    let state = State::new();
-    let router = router! {
-        index: get "/" => handler,
-        by_name: get "/events/:name/" => state
-    };
     let settings = read_settings().expect("failed to read settings");
+    let table = Mutex::new(EventTable::new());
     //email::connect(&settings).expect("could not connect to email provider");
-    Iron::new(router).http("localhost:8000").unwrap();
+    rocket::ignite().manage(table);
 }
 
 fn read_settings() -> Result<Settings, Error> {
